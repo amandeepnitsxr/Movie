6f623a260a16847b7ee81679aa5eb576459c0225
amandeep dogra <amandeep.nitsri@gmail.com>
2017-08-01 23:17:02 +0530

    remove inside out

--- entertain.py
+++ entertain.py
@@ -6,7 +6,7 @@ import media
 
 toy_story = media.Movie("Toy Story","A story fo a boy whose toys came into life","http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg","https://www.youtube.com/watch?v=KYz2wyBy3kc")
 avatar = media.Movie("Avatar","A mariane on alien planet","http://www.impawards.com/2009/posters/avatar_xlg.jpg","https://www.youtube.com/watch?v=5PSNL1qE6VY")
-inside_out = media.Movie("Inside Out","A story about emotions and mind","https://images-na.ssl-images-amazon.com/images/I/51g%2B1-GJCuL._SL500_AC_SS350_.jpg","https://www.youtube.com/watch?v=yRUAzGQ3nSY")
+
 up = media.Movie("Up","A old man's adventure","https://a.dilcdn.com/bl/wp-content/uploads/sites/25/2014/01/up-poster.jpg","https://www.youtube.com/watch?v=2PD7qi8VK_o")
 
 
